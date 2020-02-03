2a09aa55ab058f421e9a5c59bd99904a65f69732
Brayan <brayanleal21@hotmail.com>
2020-02-03 15:26:58 -0600

    Model

---
+++ Lab1/Lab1/Models/Bebida.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab1.Models
+{
+    public class Bebida
+    {
+        public String Name { get; set; }
+        public String Flavor { get; set; }
+        public int Volume { get; set; }
+        public float Price { get; set; }
+        public String HouseM { get; set; }
+    }
+}
