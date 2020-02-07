eaebd80650a88eb5dd3e2bf74a6d7d7bb23ff769
Brayan <brayanleal21@hotmail.com>
2020-02-07 09:43:43 -0600

    Model bebida

---
+++ Lab1/Lab1/Models/Bebidas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1.Models
+{
+    public class Bebidas
+    {
+        /*
+         * Nombre (único) • Sabor • Volumen • Precio • Casa productora
+         */
+        public String Nombre { get; set; }
+        public String Sabor { get; set; }
+        public int Volumen { get; set; }
+        public float Precio { get; set; }
+        public String Casa { get; set; }
+    }
+}
